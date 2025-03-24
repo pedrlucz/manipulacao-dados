e59599a480d7c1c40c8e8944fbe3969c74da7dd9
pedrlucz <pedrlucz7@gmail.com>
2025-03-24 16:00:42 -0300

    .env

--- dbconfig.py
+++ dbconfig.py
@@ -1,6 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '123456',
-    'database': 'leitura'
-}
+                'host': os.getenv("DB_HOST"),
+                    'user': os.getenv("DB_USER"),
+                        'password': os.getenv("DB_PASSWORD"),
+                            'database': os.getenv("DB_NAME")
+                                                                }
