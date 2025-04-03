21e5b36e56114446a2f14c5917ec225134b60d2c
pedrlucz <pedrlucz7@gmail.com>
2025-04-03 11:53:04 -0300

    Revert "subindo api"
    This reverts commit 7ffa54aa1ee8ddd2405683aa45a4a4e916351622.

--- main.py
+++ main.py
@@ -1,55 +1,10 @@
-from funcions import *
-from flask import request, jsonify
-from werkzeug.utils import secure_filename
-from dbconfig import *
+from funcions import processar_arquivos
 import logging
-import os
 
-app = create_app()
+# pasta onde dos arquivos CSV 
+pasta_leads = r'.\leituraleads'
 
-ALLOWED_EXTENSIONS = {'csv'} # extensão permitida
-
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS # se depois do . vai ta escrito csv
-
-@app.route('/proccess', methods = ['POST'])
-def processo_arquivo():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return jsonify({"error": "Nenhum arquivo enviado"}), 400
-        
-        file = request.files['file']
-        
-        if file.filename == '':
-            return jsonify({"error": "Nome inválido"}), 400
-        
-        if not allowed_file(file.filename):
-            return jsonify({"error": "Extensão não permitida (use um arquivo .csv)"}), 400
-        
-        filename = secure_filename(file.filename)
-        
-        # pasta onde dos arquivos CSV 
-        pasta_leads = r'.\leituraleads'
-
-        if not os.path.exists(pasta_leads):
-            os.makedirs(pasta_leads)
-
-        caminho_arquivo = os.path.join(pasta_leads, filename)
-        file.save(caminho_arquivo)
-
-        logging.info('Processo Iniciado')
-        
-        try:        
-            processar_arquivos(pasta_leads)
-            logging.info('Processo Concluído')
-            
-            return jsonify({"status": True, "message": "Processo Concluído"}), 200
-        
-        except Exception as e:
-            logging.error(f"Erro ao processar arquivos: {str(e)}")
-        
-        return jsonify({f"error": "erro interno ao processar arquivos"}), 500
-
-if __name__ == "__main__":
-    app.run(host = '192.168.20.139', port = 8000, debug = True)
+# logging pra gerir os logs
+logging.info('Processo Iniciado')
+processar_arquivos(pasta_leads)
+logging.info('Processo Concluído')
