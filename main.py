6c2358c2cd1808a15e5b7134c5f0d6abe86cda79
pedrlucz <pedrlucz7@gmail.com>
2025-04-09 11:47:14 -0300

    para o servidor

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ def processo_arquivo():
         return jsonify({f"error": "erro interno ao processar arquivos"}), 500
 
 if __name__ == "__main__":
-    app.run(host = '192.168.20.139', port = 8000, debug = True)
+    app.run(host = 'localhost', port = 5000)
