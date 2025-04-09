a5bb0288c54fad0eca99d1cb6dfc2276f97b15b3
pedrlucz <pedrlucz7@gmail.com>
2025-04-09 11:47:55 -0300

    modelo

--- dbconfig.py
+++ dbconfig.py
@@ -1,7 +1,27 @@
 from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 import os
 
 load_dotenv()
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    load_dotenv()
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    
+    # inicialize o db com o app
+    db.init_app(app)
+
+    # inicializa as tabelas
+    with app.app_context():
+        db.create_all()
+
+    return app
 
 db_config = {
                 'host': os.getenv("DB_HOST"),
